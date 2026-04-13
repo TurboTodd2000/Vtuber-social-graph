04f8326e01cef7ce1e1de2b65fe36bf29fe98a62
TurboTodd2000 <113813558+TurboTodd2000@users.noreply.github.com>
2026-04-13 06:45:53 -0700

    inital push
    putting files up

---
+++ README.md
@@ -0,0 +1,6 @@
+# Vis Network | Data | Dynamic filtering
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/TurboTodd2k/pen/RNGevLK](https://codepen.io/TurboTodd2k/pen/RNGevLK).
+
---
+++ dist/script_v2.js
@@ -0,0 +1,50 @@
+	
+	{ id: 1, label: "Ironmouse", group: "Vshojo" },
+
+
+
+
+// interest groups
+{ id: 3000, label: "Yapping",  group: "interests", GroupNode: 2, size: 30, shape: "dot" },
+{ id: 3001, label: "Gaming",  group: "interests", GroupNode: 2, size: 30, shape: "dot" },
+{ id: 3002, label: "Music",  group: "interests", GroupNode: 2, size: 30, shape: "dot" },
+{ id: 3003, label: "IRL",  group: "interests", GroupNode: 2, size: 30, shape: "dot" },
+{ id: 3004, label: "Art",  group: "interests", GroupNode: 2, size: 30, shape: "dot" },
+{ id: 3005, label: "STEM",  group: "interests", GroupNode: 2, size: 30, shape: "dot" },	
+
+
+
+
+
+
+
+//INTEREST NODE
+{["Topic", "group", "size", "shape"]}
+
+
+
+
+
+
+
+//OLD ORGINIZATION NODE
+//{ id: 0, label: "ex-Vshjo", group: "Vshojo", GroupNode: 1, size: 30, shape: "dot" },
+
+
+//ORGINIZATION NODE
+{["Vshojo", "node color", "label text color", "size", "shape"],  }
+
+
+
+
+
+
+
+//TALENT node
+//name, [formal groups], [informal groups], [interests]
+{"Ironmouse", [1], ["Vein Gang"], [3000, 3001, 3002]},
+
+
+
+
+
